26324e94708248bfbad68a8d7faeb71866882a1e
DaveTheMLGPro <davethemlgpro@gmail.com>
2026-07-22 10:45:15 -0500

    init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# another-hud-mod
+Another HUD Mod is a Mod that, well, implements HUDs.
---
+++ src/client/java/net/davethemlgpro/client/AnotherHUDModClient.java
@@ -0,0 +1,10 @@
+package net.davethemlgpro.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnotherHUDModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
