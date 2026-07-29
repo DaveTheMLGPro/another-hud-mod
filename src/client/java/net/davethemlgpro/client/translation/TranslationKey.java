b16d8aa4a45032d36016be9a4053bb6307409ce8
DaveTheMLGPro <davethemlgpro@gmail.com>
2026-07-29 13:12:58 -0500

    refactor: organize translation key namespaces

--- src/client/java/net/davethemlgpro/client/translation/TranslationKey.java
+++ src/client/java/net/davethemlgpro/client/translation/TranslationKey.java
@@ -5,123 +5,124 @@ import net.minecraft.network.chat.Component;
 public enum TranslationKey {
 	KEY_CATEGORY_GENERAL("key.category.another-hud-mod.general"),
 	KEY_OPEN_LAYOUT_EDITOR("key.another-hud-mod.open_layout_editor"),
-	MODULE_ARMOR("module.another-hud-mod.armor"),
+	MODULE_ARMOR("module.another-hud-mod.armor.name"),
 	EDITOR_TITLE("screen.another-hud-mod.editor.title"),
 	EDITOR_INSTRUCTIONS("screen.another-hud-mod.editor.instructions"),
 	EDITOR_RESET("screen.another-hud-mod.editor.reset"),
 	EDITOR_SAVE_FAILED("screen.another-hud-mod.editor.save_failed"),
-	POPOVER_NO_SETTINGS("screen.another-hud-mod.popover.no_settings"),
-	SETTINGS_TAB_LAYOUT("screen.another-hud-mod.settings.tab.layout"),
-	SETTINGS_TAB_BAR("screen.another-hud-mod.settings.tab.bar"),
-	SETTINGS_TAB_TEXT("screen.another-hud-mod.settings.tab.text"),
-	SETTINGS_SECTION_LAYOUT("screen.another-hud-mod.settings.section.layout"),
-	SETTINGS_SECTION_DURABILITY_BAR("screen.another-hud-mod.settings.section.durability_bar"),
-	SETTINGS_SECTION_DURABILITY_TEXT("screen.another-hud-mod.settings.section.durability_text"),
-	SETTINGS_SECTION_LOW_DURABILITY_WARNING("screen.another-hud-mod.settings.section.low_durability_warning"),
-	SETTINGS_SECTION_COLORS("screen.another-hud-mod.settings.section.colors"),
-	SETTINGS_ARMOR_ORIENTATION("screen.another-hud-mod.settings.armor.orientation"),
-	SETTINGS_ARMOR_ORIENTATION_DESCRIPTION("screen.another-hud-mod.settings.armor.orientation.description"),
-	SETTINGS_ARMOR_SCALE("screen.another-hud-mod.settings.armor.scale"),
-	SETTINGS_ARMOR_SCALE_DESCRIPTION("screen.another-hud-mod.settings.armor.scale.description"),
-	SETTINGS_ARMOR_SPACING("screen.another-hud-mod.settings.armor.spacing"),
-	SETTINGS_ARMOR_SPACING_DESCRIPTION("screen.another-hud-mod.settings.armor.spacing.description"),
-	SETTINGS_ARMOR_SLOT_STYLE("screen.another-hud-mod.settings.armor.slot_style"),
-	SETTINGS_ARMOR_SLOT_STYLE_DESCRIPTION("screen.another-hud-mod.settings.armor.slot_style.description"),
-	SETTINGS_ARMOR_SHOW_EMPTY("screen.another-hud-mod.settings.armor.show_empty"),
-	SETTINGS_ARMOR_SHOW_EMPTY_DESCRIPTION("screen.another-hud-mod.settings.armor.show_empty.description"),
-	SETTINGS_ARMOR_CENTER_VISIBLE("screen.another-hud-mod.settings.armor.center_visible"),
-	SETTINGS_ARMOR_CENTER_VISIBLE_DESCRIPTION("screen.another-hud-mod.settings.armor.center_visible.description"),
-	SETTINGS_ARMOR_DURABILITY_BAR_VISIBLE("screen.another-hud-mod.settings.armor.durability_bar.visible"),
+	POPOVER_NO_SETTINGS("popover.another-hud-mod.no_settings"),
+	SETTINGS_TAB_LAYOUT("module.another-hud-mod.armor.settings.tab.layout"),
+	SETTINGS_TAB_BAR("module.another-hud-mod.armor.settings.tab.bar"),
+	SETTINGS_TAB_TEXT("module.another-hud-mod.armor.settings.tab.text"),
+	SETTINGS_SECTION_LAYOUT("module.another-hud-mod.armor.settings.section.layout"),
+	SETTINGS_SECTION_DURABILITY_BAR("module.another-hud-mod.armor.settings.section.durability_bar"),
+	SETTINGS_SECTION_DURABILITY_TEXT("module.another-hud-mod.armor.settings.section.durability_text"),
+	SETTINGS_SECTION_LOW_DURABILITY_WARNING(
+		"module.another-hud-mod.armor.settings.section.low_durability_warning"),
+	SETTINGS_SECTION_COLORS("module.another-hud-mod.armor.settings.section.colors"),
+	SETTINGS_ARMOR_ORIENTATION("module.another-hud-mod.armor.settings.orientation"),
+	SETTINGS_ARMOR_ORIENTATION_DESCRIPTION("module.another-hud-mod.armor.settings.orientation.description"),
+	SETTINGS_ARMOR_SCALE("module.another-hud-mod.armor.settings.scale"),
+	SETTINGS_ARMOR_SCALE_DESCRIPTION("module.another-hud-mod.armor.settings.scale.description"),
+	SETTINGS_ARMOR_SPACING("module.another-hud-mod.armor.settings.spacing"),
+	SETTINGS_ARMOR_SPACING_DESCRIPTION("module.another-hud-mod.armor.settings.spacing.description"),
+	SETTINGS_ARMOR_SLOT_STYLE("module.another-hud-mod.armor.settings.slot_style"),
+	SETTINGS_ARMOR_SLOT_STYLE_DESCRIPTION("module.another-hud-mod.armor.settings.slot_style.description"),
+	SETTINGS_ARMOR_SHOW_EMPTY("module.another-hud-mod.armor.settings.show_empty"),
+	SETTINGS_ARMOR_SHOW_EMPTY_DESCRIPTION("module.another-hud-mod.armor.settings.show_empty.description"),
+	SETTINGS_ARMOR_CENTER_VISIBLE("module.another-hud-mod.armor.settings.center_visible"),
+	SETTINGS_ARMOR_CENTER_VISIBLE_DESCRIPTION("module.another-hud-mod.armor.settings.center_visible.description"),
+	SETTINGS_ARMOR_DURABILITY_BAR_VISIBLE("module.another-hud-mod.armor.settings.durability_bar.visible"),
 	SETTINGS_ARMOR_DURABILITY_BAR_VISIBLE_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_bar.visible.description"),
-	SETTINGS_ARMOR_DURABILITY_BAR_HEIGHT("screen.another-hud-mod.settings.armor.durability_bar.height"),
+		"module.another-hud-mod.armor.settings.durability_bar.visible.description"),
+	SETTINGS_ARMOR_DURABILITY_BAR_HEIGHT("module.another-hud-mod.armor.settings.durability_bar.height"),
 	SETTINGS_ARMOR_DURABILITY_BAR_HEIGHT_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_bar.height.description"),
-	SETTINGS_ARMOR_DURABILITY_BAR_PADDING("screen.another-hud-mod.settings.armor.durability_bar.padding"),
+		"module.another-hud-mod.armor.settings.durability_bar.height.description"),
+	SETTINGS_ARMOR_DURABILITY_BAR_PADDING("module.another-hud-mod.armor.settings.durability_bar.padding"),
 	SETTINGS_ARMOR_DURABILITY_BAR_PADDING_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_bar.padding.description"),
+		"module.another-hud-mod.armor.settings.durability_bar.padding.description"),
 	SETTINGS_ARMOR_DURABILITY_BAR_BACKGROUND_COLOR(
-		"screen.another-hud-mod.settings.armor.durability_bar.background_color"),
+		"module.another-hud-mod.armor.settings.durability_bar.background_color"),
 	SETTINGS_ARMOR_DURABILITY_BAR_BACKGROUND_COLOR_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_bar.background_color.description"),
+		"module.another-hud-mod.armor.settings.durability_bar.background_color.description"),
 	SETTINGS_ARMOR_DURABILITY_BAR_HEALTHY_COLOR(
-		"screen.another-hud-mod.settings.armor.durability_bar.healthy_color"),
+		"module.another-hud-mod.armor.settings.durability_bar.healthy_color"),
 	SETTINGS_ARMOR_DURABILITY_BAR_HEALTHY_COLOR_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_bar.healthy_color.description"),
+		"module.another-hud-mod.armor.settings.durability_bar.healthy_color.description"),
 	SETTINGS_ARMOR_DURABILITY_BAR_WARNING_COLOR(
-		"screen.another-hud-mod.settings.armor.durability_bar.warning_color"),
+		"module.another-hud-mod.armor.settings.durability_bar.warning_color"),
 	SETTINGS_ARMOR_DURABILITY_BAR_WARNING_COLOR_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_bar.warning_color.description"),
+		"module.another-hud-mod.armor.settings.durability_bar.warning_color.description"),
 	SETTINGS_ARMOR_DURABILITY_BAR_CRITICAL_COLOR(
-		"screen.another-hud-mod.settings.armor.durability_bar.critical_color"),
+		"module.another-hud-mod.armor.settings.durability_bar.critical_color"),
 	SETTINGS_ARMOR_DURABILITY_BAR_CRITICAL_COLOR_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_bar.critical_color.description"),
-	SETTINGS_ARMOR_DURABILITY_MODE("screen.another-hud-mod.settings.armor.durability_text.mode"),
+		"module.another-hud-mod.armor.settings.durability_bar.critical_color.description"),
+	SETTINGS_ARMOR_DURABILITY_MODE("module.another-hud-mod.armor.settings.durability_text.mode"),
 	SETTINGS_ARMOR_DURABILITY_MODE_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_text.mode.description"),
-	SETTINGS_ARMOR_DURABILITY_TEXT_POSITION("screen.another-hud-mod.settings.armor.durability_text.position"),
+		"module.another-hud-mod.armor.settings.durability_text.mode.description"),
+	SETTINGS_ARMOR_DURABILITY_TEXT_POSITION(
+		"module.another-hud-mod.armor.settings.durability_text.position"),
 	SETTINGS_ARMOR_DURABILITY_TEXT_POSITION_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_text.position.description"),
-	SETTINGS_ARMOR_DURABILITY_TEXT_SCALE("screen.another-hud-mod.settings.armor.durability_text.scale"),
+		"module.another-hud-mod.armor.settings.durability_text.position.description"),
+	SETTINGS_ARMOR_DURABILITY_TEXT_SCALE("module.another-hud-mod.armor.settings.durability_text.scale"),
 	SETTINGS_ARMOR_DURABILITY_TEXT_SCALE_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_text.scale.description"),
-	SETTINGS_ARMOR_DURABILITY_TEXT_SHADOW("screen.another-hud-mod.settings.armor.durability_text.shadow"),
+		"module.another-hud-mod.armor.settings.durability_text.scale.description"),
+	SETTINGS_ARMOR_DURABILITY_TEXT_SHADOW("module.another-hud-mod.armor.settings.durability_text.shadow"),
 	SETTINGS_ARMOR_DURABILITY_TEXT_SHADOW_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_text.shadow.description"),
+		"module.another-hud-mod.armor.settings.durability_text.shadow.description"),
 	SETTINGS_ARMOR_COLOR_BASED_DURABILITY_TEXT(
-		"screen.another-hud-mod.settings.armor.durability_text.color_based"),
+		"module.another-hud-mod.armor.settings.durability_text.color_based"),
 	SETTINGS_ARMOR_COLOR_BASED_DURABILITY_TEXT_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_text.color_based.description"),
-	SETTINGS_ARMOR_DURABILITY_TEXT_COLOR(
-		"screen.another-hud-mod.settings.armor.durability_text.color"),
+		"module.another-hud-mod.armor.settings.durability_text.color_based.description"),
+	SETTINGS_ARMOR_DURABILITY_TEXT_COLOR("module.another-hud-mod.armor.settings.durability_text.color"),
 	SETTINGS_ARMOR_DURABILITY_TEXT_COLOR_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_text.color.description"),
+		"module.another-hud-mod.armor.settings.durability_text.color.description"),
 	SETTINGS_ARMOR_DURABILITY_TEXT_HEALTHY_COLOR(
-		"screen.another-hud-mod.settings.armor.durability_text.healthy_color"),
+		"module.another-hud-mod.armor.settings.durability_text.healthy_color"),
 	SETTINGS_ARMOR_DURABILITY_TEXT_HEALTHY_COLOR_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_text.healthy_color.description"),
+		"module.another-hud-mod.armor.settings.durability_text.healthy_color.description"),
 	SETTINGS_ARMOR_DURABILITY_TEXT_WARNING_COLOR(
-		"screen.another-hud-mod.settings.armor.durability_text.warning_color"),
+		"module.another-hud-mod.armor.settings.durability_text.warning_color"),
 	SETTINGS_ARMOR_DURABILITY_TEXT_WARNING_COLOR_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_text.warning_color.description"),
+		"module.another-hud-mod.armor.settings.durability_text.warning_color.description"),
 	SETTINGS_ARMOR_DURABILITY_TEXT_CRITICAL_COLOR(
-		"screen.another-hud-mod.settings.armor.durability_text.critical_color"),
+		"module.another-hud-mod.armor.settings.durability_text.critical_color"),
 	SETTINGS_ARMOR_DURABILITY_TEXT_CRITICAL_COLOR_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.durability_text.critical_color.description"),
+		"module.another-hud-mod.armor.settings.durability_text.critical_color.description"),
 	SETTINGS_ARMOR_LOW_DURABILITY_WARNING_VISIBLE(
-		"screen.another-hud-mod.settings.armor.low_durability_warning.visible"),
+		"module.another-hud-mod.armor.settings.low_durability_warning.visible"),
 	SETTINGS_ARMOR_LOW_DURABILITY_WARNING_VISIBLE_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.low_durability_warning.visible.description"),
+		"module.another-hud-mod.armor.settings.low_durability_warning.visible.description"),
 	SETTINGS_ARMOR_LOW_DURABILITY_THRESHOLD(
-		"screen.another-hud-mod.settings.armor.low_durability_warning.threshold"),
+		"module.another-hud-mod.armor.settings.low_durability_warning.threshold"),
 	SETTINGS_ARMOR_LOW_DURABILITY_THRESHOLD_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.low_durability_warning.threshold.description"),
+		"module.another-hud-mod.armor.settings.low_durability_warning.threshold.description"),
 	SETTINGS_ARMOR_LOW_DURABILITY_WARNING_STYLE(
-		"screen.another-hud-mod.settings.armor.low_durability_warning.style"),
+		"module.another-hud-mod.armor.settings.low_durability_warning.style"),
 	SETTINGS_ARMOR_LOW_DURABILITY_WARNING_STYLE_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.low_durability_warning.style.description"),
+		"module.another-hud-mod.armor.settings.low_durability_warning.style.description"),
 	SETTINGS_ARMOR_LOW_DURABILITY_WARNING_COLOR(
-		"screen.another-hud-mod.settings.armor.low_durability_warning.color"),
+		"module.another-hud-mod.armor.settings.low_durability_warning.color"),
 	SETTINGS_ARMOR_LOW_DURABILITY_WARNING_COLOR_DESCRIPTION(
-		"screen.another-hud-mod.settings.armor.low_durability_warning.color.description"),
-	COLOR_PICKER_ALPHA("screen.another-hud-mod.color_picker.alpha"),
-	COLOR_PICKER_INPUT("screen.another-hud-mod.color_picker.input"),
-	SETTINGS_VALUE_HORIZONTAL("screen.another-hud-mod.settings.value.horizontal"),
-	SETTINGS_VALUE_VERTICAL("screen.another-hud-mod.settings.value.vertical"),
-	SETTINGS_VALUE_CLEAR("screen.another-hud-mod.settings.value.clear"),
-	SETTINGS_VALUE_INVENTORY("screen.another-hud-mod.settings.value.inventory"),
-	SETTINGS_VALUE_HOTBAR("screen.another-hud-mod.settings.value.hotbar"),
-	SETTINGS_VALUE_NONE("screen.another-hud-mod.settings.value.none"),
-	SETTINGS_VALUE_VALUE("screen.another-hud-mod.settings.value.value"),
-	SETTINGS_VALUE_PERCENT("screen.another-hud-mod.settings.value.percent"),
-	SETTINGS_VALUE_TOP("screen.another-hud-mod.settings.value.top"),
-	SETTINGS_VALUE_BOTTOM("screen.another-hud-mod.settings.value.bottom"),
-	SETTINGS_VALUE_LEFT("screen.another-hud-mod.settings.value.left"),
-	SETTINGS_VALUE_RIGHT("screen.another-hud-mod.settings.value.right"),
-	SETTINGS_VALUE_CENTER("screen.another-hud-mod.settings.value.center"),
-	SETTINGS_VALUE_COLOR("screen.another-hud-mod.settings.value.color"),
-	SETTINGS_VALUE_PULSE("screen.another-hud-mod.settings.value.pulse"),
-	SETTINGS_VALUE_FLASH("screen.another-hud-mod.settings.value.flash");
+		"module.another-hud-mod.armor.settings.low_durability_warning.color.description"),
+	COLOR_PICKER_ALPHA("popover.another-hud-mod.color_picker.alpha"),
+	COLOR_PICKER_INPUT("popover.another-hud-mod.color_picker.input"),
+	SETTINGS_VALUE_HORIZONTAL("option.another-hud-mod.horizontal"),
+	SETTINGS_VALUE_VERTICAL("option.another-hud-mod.vertical"),
+	SETTINGS_VALUE_CLEAR("option.another-hud-mod.clear"),
+	SETTINGS_VALUE_INVENTORY("option.another-hud-mod.inventory"),
+	SETTINGS_VALUE_HOTBAR("option.another-hud-mod.hotbar"),
+	SETTINGS_VALUE_NONE("option.another-hud-mod.none"),
+	SETTINGS_VALUE_VALUE("option.another-hud-mod.value"),
+	SETTINGS_VALUE_PERCENT("option.another-hud-mod.percent"),
+	SETTINGS_VALUE_TOP("option.another-hud-mod.top"),
+	SETTINGS_VALUE_BOTTOM("option.another-hud-mod.bottom"),
+	SETTINGS_VALUE_LEFT("option.another-hud-mod.left"),
+	SETTINGS_VALUE_RIGHT("option.another-hud-mod.right"),
+	SETTINGS_VALUE_CENTER("option.another-hud-mod.center"),
+	SETTINGS_VALUE_COLOR("option.another-hud-mod.color"),
+	SETTINGS_VALUE_PULSE("option.another-hud-mod.pulse"),
+	SETTINGS_VALUE_FLASH("option.another-hud-mod.flash");
 
 	private final String key;
 
